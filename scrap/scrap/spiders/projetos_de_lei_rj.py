386d0e36b2441a8fbd1d834b1bddc4568a0c3ea4
Fred Israel <fredpublico@gmail.com>
2015-01-16 16:33:31 -0200

    master - ignoring *.pyc files

--- scrap/scrap/spiders/projetos_de_lei_rj.py
+++ scrap/scrap/spiders/projetos_de_lei_rj.py
@@ -2,6 +2,7 @@
 import scrapy
 import scrap.items as items
 from urlparse import urlparse
+from bs4 import BeautifulSoup
 
 class ProjetosDeLeiRjSpider(scrapy.Spider):
 	name = "projetos_de_lei_rj"
@@ -13,15 +14,17 @@ class ProjetosDeLeiRjSpider(scrapy.Spider):
 	def parse(self, response):
 		for line in response.css('body>form>table')[1].css('tr'):
 			if line.css('img[alt*="Red right arrow Icon"]'):
-				url = line.css('a').xpath('@href')[0].extract()
+				url = line.css('a').xpath('@href')[-1].extract()
 				base = urlparse(response.url)
 				yield scrapy.Request(base_url(base, url), callback=self.parse_law_page)
 				break
 
 	def parse_law_page(self, response):
 		lei = items.ProjetoDeLei()
+		html = BeautifulSoup(response.body)
+		text = html.get_text()
 		import ipdb; ipdb.set_trace()
-		lei['ementa'] = response.text
+		lei['ementa']
 		yield lei
 
 def base_url(base, uri):
