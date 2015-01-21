6e8bbbfe99f744ac421fe4c939d744b656ffa03d
Ricardo Dahis <rdahis@gmail.com>
2015-01-21 11:18:06 -0200

    Spiders novas, mas não 100% funcionando ainda.

--- scrap/scrap/items.py
+++ scrap/scrap/items.py
@@ -13,15 +13,11 @@ from scrapy import Field
 # Politics
 #----------------------------------------------------------------#
 
-class ProjetoDeLei(scrapy.Item):
-	# define the fields for your item here like:
-	# name = scrapy.Field()
-	ementa = Field()
-	autores = Field()
-	corpo = Field()
+#----------------------#
+# Camara dos Deputados
+#----------------------#
 
-
-class DeputadoFederal(scrapy.Item):
+class DeputadoCamara(scrapy.Item):
     id_cadastro = Field()
     id_deputado_federal = Field()
     id_orcamento = Field()
@@ -36,6 +32,49 @@ class DeputadoFederal(scrapy.Item):
     anexo = Field()
     telefone = Field()
     url_foto = Field()
+
+class LiderBancada(scrapy.Item):
+    nome = Field()
+    id_cadastro = Field()
+    partido = Field()
+    uf = Field()
+
+class PartidoCamara(scrapy.Item):
+    id_partido_camara = Field()
+    partido = Field()
+    partido_nome = Field()
+    partido_data_criacao = Field()
+    partido_data_extincao = Field()
+
+class OrgaoCargoCamara(scrapy.Item):
+    id_cargo = Field()
+    descricao = Field()
+
+class OrgaoCamara(scrapy.Item):
+    id_orgao = Field()
+    descricao = Field()
+
+#---------------------------------------#
+# Camara de Vereadores - Rio de Janeiro
+#---------------------------------------#
+
+class ProjetoDeLei(scrapy.Item):
+	# define the fields for your item here like:
+	# name = scrapy.Field()
+	ementa = Field()
+	autores = Field()
+	corpo = Field()
+
+#Cargos
+
+#Proposicoes
+
+#Sessoes/Reunioes
+#    Lista Presenca Parlamentares (DataIni, DataFim, matricula)
+
+
+
+
 
 
 
---
+++ scrap/scrap/spiders/camara_orgaos.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import scrap.items as items
+
+# XML Parser
+import elementtree.ElementTree as ET
+from elementtree.ElementTree import Element, SubElement
+
+
+class CamaraOrgaosSpider(scrapy.Spider):
+    name = "camara_orgaos"
+    allowed_domains = ["www2.camara.leg.br/"]
+    start_urls = (
+        'http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ListarTiposOrgaos',
+    )
+
+    def parse(self, response):
+        tree = ET.fromstring(response.body)
+        for orgao in tree.find('./tipoOrgao'):
+            i = _create_item_from_element(orgao)
+            yield i
+
+def _create_item_from_element(element):
+    out = items.OrgaoCamara()
+    out['id_orgao'] = element.attrib['id']
+    out['descricao'] = element.attrib['descricao']
+    return out
