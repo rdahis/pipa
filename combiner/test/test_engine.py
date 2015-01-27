e68f0ff311d5ba8cb7ef7a150fc7118dc84d247b
Fred Israel <fredpublico@gmail.com>
2015-01-27 15:29:18 -0200

    small

--- combiner/test/test_engine.py
+++ combiner/test/test_engine.py
@@ -1,5 +1,5 @@
 import unittest
 import combiner.engine
-import combiner.test.test_combiner as test_combiner
+import combiner.test.fake_combiner as fake_combiner
 aa = combiner.engine.run_combiner(test_combiner)
 import ipdb;ipdb.set_trace()
