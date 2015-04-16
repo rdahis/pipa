9cd716163b4ae5aade95de780f9b4853fa31222a
Fred Israel <fredpublico@gmail.com>
2015-04-16 14:18:28 -0300

    wip

--- cnj.py
+++ cnj.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 
-webdriver = webdriver.Firefox()
-webdriver.get('http://www.cnj.jus.br/corregedoria/justica_aberta/?')
+driver = webdriver.Firefox()
+url = 'http://www.cnj.jus.br/corregedoria/justica_aberta/?'
+driver.get(url)
+assert driver.current_url == url
+menu = driver.find_element_by_xpath("//div[@class='menu navbar']")
+menu.find_elements_by_tag_name('button').click()
+links = menu.find_elements_by_tag_name('a') 
+import ipdb; ipdb.set_trace()
