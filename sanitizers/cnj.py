c82c4b5d3d2b4dc2ad0b864cb330f2a7abbc589d
Ricardo Dahis <rdahis@gmail.com>
2015-04-21 15:49:09 -0300

    sanitizer maneiro

--- sanitizers/cnj.py
+++ sanitizers/cnj.py
@@ -1,40 +1,82 @@
+import pdfquery
+import re
 
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from cStringIO import StringIO
+import json
 
 path = '/Users/ricardodahis/Desktop/raw_cnj'
+path_pdf = '/Users/ricardodahis/Desktop/raw_cnj/agudo_1_2009.pdf'
+
 def main():
-	words = convert_pdf_to_txt(path+'/agudo_1_2009.pdf')
-	with open(path+'/output.txt', 'w') as text_file:
-		text_file.write(words)
+	text = convert_pdf_to_txt(path+'/agudo_1_2009.pdf')
+	#with open(path+'/output.txt', 'w') as text_file:
+	#	text_file.write(words)
+	parser = Parser()
+	a,b = parser.parse(text)
+	from pprint import pprint
+	print(json.dumps(a, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
+	print(json.dumps(b, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+
+
+class Parser():
+	def __init__(self):
+		self.state = self._initial
+		self.header = {}
+		self.out = {}
+		self.out_keys = []
+
+	def _initial(self, line):
+		if ':' in line:
+			self.state = self._header
+			self._header(line)
+	
+	def _header(self, line):
+		if 'QUESTION' in line and ':' not in line:
+			self.state = self._data
+			return
+		key, value = line.split(':')
+		self.header[key.strip()] = value.strip()
+
+	def _data(self, line):
+		regexp = re.compile('^[0-9]* *')
+		line = re.sub(regexp, '', line)
+		if 'Total' == line:
+			self.state = self._data_values
+		if not line or ':' not in line: return
+		key, _ = line.split(':')
+		self.out_keys.append(key)
+
+	def _data_values(self, line):
+		if 'Corregedoria Nacional' in line:
+			self.state = None
+			return
+		value = int(line)
+		key = self.out_keys.pop(0) # FIFO
+		self.out[key.strip()] = value
+		
+	def parse(self, text):
+		if type(text) == str:
+			text = text.split('\n')
+		for line in text:
+			line = line.strip()
+			if line and self.state:
+				try: self.state(line)
+				except Exception as ee:
+					import ipdb; ipdb.set_trace()
+					raise
+		return self.header, self.out
 
 def convert_pdf_to_txt(path):
-	rsrcmgr = PDFResourceManager()
-	retstr = StringIO()
-	codec = 'utf-8'
-	laparams = LAParams()
-	device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-	
-	fp = file(path, 'rb')
-	interpreter = PDFPageInterpreter(rsrcmgr, device)
-	password = ""
-	maxpages = 0
-	caching = True
-	pagenos=set()
-	for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-		interpreter.process_page(page)
-	fp.close()
-	device.close()
-	
-	str = retstr.getvalue()
-	retstr.close()
-	return str
-
-
+	pdf = pdfquery.PDFQuery(path)	
+	pdf.load()
+	tree = pdf.get_tree()
+	root = tree.getroot()
+	text = []
+	for el in root.iterdescendants():
+		if not el.text: continue
+		text.append(el.text)
+	return text
 
 if __name__ == '__main__':
 	main()
-
